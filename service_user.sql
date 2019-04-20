2203e6abc6ceb8f13cfc2be69a4d97a6b3181650
Dang Ngoc Luan <luandn@toshiba-tsdv.com>
2019-04-20 15:40:05 +0700

    add

--- service_user.sql
+++ service_user.sql
@@ -1,6 +1,8 @@
 create table if not exists service_user
-(
+(	id bigint not null,
 	insurer_number varchar(20) not null,
+	insured_number varchar(20) not null,
+	service_user_number varchar(6) not null,
 	name varchar(40) not null,
 	gender varchar(20),
 	birthday varchar(12),
@@ -8,10 +10,25 @@ create table if not exists service_user
 	phone_number varchar(20),
 	care_period_start_date varchar(12),
 	care_period_end_date varchar(12),
-	profile_picture varchar(5000),
-	id bigint not null,
+	profile_picture varchar(100000)
+);
+
+create table if not exists service_user_history
+(	id bigint not null,
+	insurer_number varchar(20) not null,
 	insured_number varchar(20) not null,
-	service_user_number varchar(6)
+	service_user_number varchar(6) not null,
+	name varchar(40) not null,
+	gender varchar(20),
+	birthday varchar(12),
+	address varchar(40),
+	phone_number varchar(20),
+	care_period_start_date varchar(12),
+	care_period_end_date varchar(12),
+	profile_picture varchar(100000),
+	inserted varchar(40),
+	insert_time timestamp 
+	
 );
 
 INSERT INTO public.service_user (insurer_number, name, gender, birthday, address, phone_number, care_period_start_date, care_period_end_date, profile_picture, id, insured_number, service_user_number) VALUES ('10005', 'height', '', '1978/01/01', 'HaNoi', '0331234568', '2018/10/10', '2018/04/04', null, 6, '0', '0004');
