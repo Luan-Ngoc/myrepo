5cfa62cf52ab0043a3caf74782826059a8bdb5ba
Luan-Ngoc <luan.dangngoc@gmail.com>
2019-04-23 10:41:24 +0700

    Update service_user.sql

--- service_user.sql
+++ service_user.sql
@@ -31,14 +31,27 @@ create table if not exists service_user_history
 	insert_time timestamp 
 );
 
+INSERT INTO public.service_user (insurer_number, name, gender, birthday, address, phone_number, care_period_start_date, care_period_end_date, profile_picture, id, insured_number, service_user_number) VALUES ('10005', 'height', '', '1978/01/01', 'HaNoi', '0331234568', '2018/10/10', '2018/04/04', null, 6, '0', '0004');
+INSERT INTO public.service_user (insurer_number, name, gender, birthday, address, phone_number, care_period_start_date, care_period_end_date, profile_picture, id, insured_number, service_user_number) VALUES ('10004', 'danm', '', '1978/01/01', 'HaNoi', '0331234568', '2018/10/10', '2018/04/04', null, 4, '0', '0002');
+INSERT INTO public.service_user (insurer_number, name, gender, birthday, address, phone_number, care_period_start_date, care_period_end_date, profile_picture, id, insured_number, service_user_number) VALUES ('10005', 'height', '', '1978/01/01', 'HaNoi', '0331234568', '2018/10/10', '2018/04/04', null, 5, '0', '0003');
+INSERT INTO public.service_user (insurer_number, name, gender, birthday, address, phone_number, care_period_start_date, care_period_end_date, profile_picture, id, insured_number, service_user_number) VALUES ('10003', 'hight', '', '1978/01/01', 'HaNoi', '0331234568', '2018/10/10', '2018/04/04', null, 3, '0', '0001');
+INSERT INTO public.service_user (insurer_number, name, gender, birthday, address, phone_number, care_period_start_date, care_period_end_date, profile_picture, id, insured_number, service_user_number) VALUES ('10006', 'yellow', '', '1978/01/01', 'HaNoi', '0331234568', '2018/10/10', '2018/04/04', null, 1, '0', '0004');
+INSERT INTO public.service_user (insurer_number, name, gender, birthday, address, phone_number, care_period_start_date, care_period_end_date, profile_picture, id, insured_number, service_user_number) VALUES ('10007', 'small', '', '1978/01/01', 'HaNoi', '0331234568', '2018/10/10', '2018/04/04', null, 2, '0', '0002');
+INSERT INTO public.service_user (insurer_number, name, gender, birthday, address, phone_number, care_period_start_date, care_period_end_date, profile_picture, id, insured_number, service_user_number) VALUES ('10008', 'height', '', '1978/01/01', 'HaNoi', '0331234568', '2018/10/10', '2018/04/04', null, 7, '0', '0003');
+INSERT INTO public.service_user (insurer_number, name, gender, birthday, address, phone_number, care_period_start_date, care_period_end_date, profile_picture, id, insured_number, service_user_number) VALUES ('10009', 'hight', '', '1978/01/01', 'HaNoi', '0331234568', '2018/10/10', '2018/04/04', null, 8, '0', '0001');
+INSERT INTO public.service_user (insurer_number, name, gender, birthday, address, phone_number, care_period_start_date, care_period_end_date, profile_picture, id, insured_number, service_user_number) VALUES ('10010', 'height', '', '1978/01/01', 'HaNoi', '0331234568', '2018/10/10', '2018/04/04', null, 9, '0', '0004');
+INSERT INTO public.service_user (insurer_number, name, gender, birthday, address, phone_number, care_period_start_date, care_period_end_date, profile_picture, id, insured_number, service_user_number) VALUES ('10011', 'danm', '', '1978/01/01', 'HaNoi', '0331234568', '2018/10/10', '2018/04/04', null, 10, '0', '0002');
+INSERT INTO public.service_user (insurer_number, name, gender, birthday, address, phone_number, care_period_start_date, care_period_end_date, profile_picture, id, insured_number, service_user_number) VALUES ('10012', 'height', '', '1978/01/01', 'HaNoi', '0331234568', '2018/10/10', '2018/04/04', null, 11, '0', '0003');
+INSERT INTO public.service_user (insurer_number, name, gender, birthday, address, phone_number, care_period_start_date, care_period_end_date, profile_picture, id, insured_number, service_user_number) VALUES ('10013', 'hight', '', '1978/01/01', 'HaNoi', '0331234568', '2018/10/10', '2018/04/04', null, 12, '0', '0001');
+
 CREATE OR REPLACE FUNCTION service_user_trig_history() RETURNS trigger
   LANGUAGE plpgsql AS
 $$BEGIN
   INSERT INTO public.service_user_history (
             id,
+	    history_number,
             insurer_number,
             insured_number,
-	    history_number,
             service_user_number,
             name,
             gender,
@@ -53,9 +66,9 @@ $$BEGIN
           )
           VALUES (
             NEW.id,
+	    (SELECT COALESCE(MAX(history_number), 0) + 1 from service_user_history where id = NEW.id),
             NEW.insurer_number,
             NEW.insured_number,
-            (SELECT COALESCE(MAX(history_number), 0) + 1 from service_user_history where id = NEW.id),
             NEW.service_user_number,
             NEW.name,
             NEW.gender,
@@ -74,18 +87,3 @@ END;$$;
 
 CREATE TRIGGER service_user_trig_history BEFORE INSERT OR UPDATE ON service_user FOR EACH ROW
 EXECUTE PROCEDURE service_user_trig_history();
-   
-   
-
-INSERT INTO public.service_user (insurer_number, name, gender, birthday, address, phone_number, care_period_start_date, care_period_end_date, profile_picture, id, insured_number, service_user_number) VALUES ('10005', 'height', '', '1978/01/01', 'HaNoi', '0331234568', '2018/10/10', '2018/04/04', null, 6, '0', '0004');
-INSERT INTO public.service_user (insurer_number, name, gender, birthday, address, phone_number, care_period_start_date, care_period_end_date, profile_picture, id, insured_number, service_user_number) VALUES ('10004', 'danm', '', '1978/01/01', 'HaNoi', '0331234568', '2018/10/10', '2018/04/04', null, 4, '0', '0002');
-INSERT INTO public.service_user (insurer_number, name, gender, birthday, address, phone_number, care_period_start_date, care_period_end_date, profile_picture, id, insured_number, service_user_number) VALUES ('10005', 'height', '', '1978/01/01', 'HaNoi', '0331234568', '2018/10/10', '2018/04/04', null, 5, '0', '0003');
-INSERT INTO public.service_user (insurer_number, name, gender, birthday, address, phone_number, care_period_start_date, care_period_end_date, profile_picture, id, insured_number, service_user_number) VALUES ('10003', 'hight', '', '1978/01/01', 'HaNoi', '0331234568', '2018/10/10', '2018/04/04', null, 3, '0', '0001');
-INSERT INTO public.service_user (insurer_number, name, gender, birthday, address, phone_number, care_period_start_date, care_period_end_date, profile_picture, id, insured_number, service_user_number) VALUES ('10006', 'yellow', '', '1978/01/01', 'HaNoi', '0331234568', '2018/10/10', '2018/04/04', null, 1, '0', '0004');
-INSERT INTO public.service_user (insurer_number, name, gender, birthday, address, phone_number, care_period_start_date, care_period_end_date, profile_picture, id, insured_number, service_user_number) VALUES ('10007', 'small', '', '1978/01/01', 'HaNoi', '0331234568', '2018/10/10', '2018/04/04', null, 2, '0', '0002');
-INSERT INTO public.service_user (insurer_number, name, gender, birthday, address, phone_number, care_period_start_date, care_period_end_date, profile_picture, id, insured_number, service_user_number) VALUES ('10008', 'height', '', '1978/01/01', 'HaNoi', '0331234568', '2018/10/10', '2018/04/04', null, 7, '0', '0003');
-INSERT INTO public.service_user (insurer_number, name, gender, birthday, address, phone_number, care_period_start_date, care_period_end_date, profile_picture, id, insured_number, service_user_number) VALUES ('10009', 'hight', '', '1978/01/01', 'HaNoi', '0331234568', '2018/10/10', '2018/04/04', null, 8, '0', '0001');
-INSERT INTO public.service_user (insurer_number, name, gender, birthday, address, phone_number, care_period_start_date, care_period_end_date, profile_picture, id, insured_number, service_user_number) VALUES ('10010', 'height', '', '1978/01/01', 'HaNoi', '0331234568', '2018/10/10', '2018/04/04', null, 9, '0', '0004');
-INSERT INTO public.service_user (insurer_number, name, gender, birthday, address, phone_number, care_period_start_date, care_period_end_date, profile_picture, id, insured_number, service_user_number) VALUES ('10011', 'danm', '', '1978/01/01', 'HaNoi', '0331234568', '2018/10/10', '2018/04/04', null, 10, '0', '0002');
-INSERT INTO public.service_user (insurer_number, name, gender, birthday, address, phone_number, care_period_start_date, care_period_end_date, profile_picture, id, insured_number, service_user_number) VALUES ('10012', 'height', '', '1978/01/01', 'HaNoi', '0331234568', '2018/10/10', '2018/04/04', null, 11, '0', '0003');
-INSERT INTO public.service_user (insurer_number, name, gender, birthday, address, phone_number, care_period_start_date, care_period_end_date, profile_picture, id, insured_number, service_user_number) VALUES ('10013', 'hight', '', '1978/01/01', 'HaNoi', '0331234568', '2018/10/10', '2018/04/04', null, 12, '0', '0001');
